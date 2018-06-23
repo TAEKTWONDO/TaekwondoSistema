00d9266e0f97adc06eb665e8c77784c337984f31
GatomanJuarez <juarez_devora_ja@hotmail.com>
2018-06-23 12:57:28 -0500

    Perceptron (Limite)

--- assets/js/perceptronMethodSimple.js
+++ assets/js/perceptronMethodSimple.js
@@ -1,5 +1,5 @@
 //Global values
-var threshold, downZero, upZero, resultZ, erroResult;
+var threshold, downZero, upZero, resultZ = 0, erroResult, count = 1;
 var entriesValues = new Array;
 var exitWish = new Array;
 var height = new Array;
@@ -8,14 +8,14 @@ function addValues(){
     threshold = 0.5;
     downZero = -1;
     upZero = 1;
-    entriesValues = [5, 2, 1];
-    exitWish = 1;
-    height = [0, -8, 2];
+    entriesValues = [1, 0.5382, 0.4, 0.498];
+    exitWish = 0;
+    height = [0, -8, 2, 4];
 }
 
-function perceptron(limit){
+function perceptron(){
     addValues();
-    for (var r = 0; r <= limit; r++){
+   do{
         resultZ = calculateZ();
         erroResult = 0;
         if (resultZ >= 0){
@@ -26,7 +26,10 @@ function perceptron(limit){
         }
         console.log("Error: " + errorResult);
         newHeight(errorResult);
-    }
+        console.log("Interacción: " + count);
+        count ++;
+        console.log(resultZ <= exitWish)
+    }while (resultZ <= exitWish);
 }
 
 //yd = exit wish, yob = downZero or upZero
@@ -48,9 +51,14 @@ function calculateZ(){
 function newHeight(errorValue){
     var wk = new Array;
     for (var o = 0; o <= height.length - 1; o++){
-        wk.push( height[o] + ((threshold * errorValue) * entriesValues[o]));
+        wk.push(height[o] + ((threshold * errorValue) * entriesValues[o]));
     }
     console.log("New heights");
+    for (var r = 0; r <= height.length - 1; r++){
+        if (wk[r] == height[r]){
+            wk[r] ++;
+        }
+    }
     height = [];
     height = wk;
     console.table(height);
