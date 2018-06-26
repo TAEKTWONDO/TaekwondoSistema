c52116da0518495cfd34a05e2939cfc5be3e94c9
GatomanJuarez <juarez_devora_ja@hotmail.com>
2018-06-26 11:57:36 -0500

    Perceptron Multicapa (Semifinalizado)

--- assets/js/perceptronMethod.js
+++ assets/js/perceptronMethod.js
@@ -1,51 +1,60 @@
-
-function perceptronSynaptic (){
+function perceptronSynaptic() {
     //Need this constants for work
-    const { Network, Trainer } = window.synaptic;
-// extend the prototype chain
+    const {
+        Network
+    } = window.synaptic;
+    // extend the prototype chain
+    Perceptron.prototype = new Network();
+    Perceptron.prototype.constructor = Perceptron;
+    // Here we definete the perceptron, that values 
+    var myPerceptron = new Perceptron(2, 3, 1);
+    // Later pass for trainer
+    /////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////
+    ///////////////////Entrenamiento/////////////////////
+    var learningRate = .3; //Bias
+    for (var i = 0; i < 90000; i++) {
+        myPerceptron.activate([0, 0]); //Entradas
+        myPerceptron.propagate(learningRate, [0]);
+        //Salida deseada
+        myPerceptron.activate([0, 1]);
+        myPerceptron.propagate(learningRate, [1]);
 
-Perceptron.prototype = new Network();
-Perceptron.prototype.constructor = Perceptron;
-// Here we definete the perceptron, that values 
-var myPerceptronTrainer = new Perceptron(2,3,1);
-// Later pass for trainer
-var myTrainer = new Trainer(myPerceptron);
-myTrainer.XOR();
-// We here calculate the new results
-var myPerceptron = new Perceptron(2,3,1);
+        myPerceptron.activate([1, 0]);
+        myPerceptron.propagate(learningRate, [1]);
 
-// Values for trained
-console.log(myPerceptronTrainer.activate([0,0])); 
-console.log(myPerceptronTrainer.activate([1,0])); 
-console.log(myPerceptronTrainer.activate([0,1])); 
-console.log(myPerceptronTrainer.activate([1,1])); 
-console.log(myPerceptronTrainer.activate([0.5,0.98145])); 
-console.log(myPerceptronTrainer.activate([-1,0.48925])); 
-console.log(myPerceptronTrainer.activate([0.459,-1])); 
-console.log(myPerceptronTrainer.activate([-1,-1])); 
-console.log("///////////////////////////////////////The new values");
-console.log(myPerceptron.activate([0.4568, 0.451469498, 0.5789]));
+        myPerceptron.activate([1, 1]);
+        myPerceptron.propagate(learningRate, [0]);
+    }
+    /////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////
+    // test the network
+    console.log(myPerceptron.activate([0, 0]));
+    console.log(myPerceptron.activate([0, 1]));
+    console.log(myPerceptron.activate([1, 0]));
+    console.log(myPerceptron.activate([1, 1]));
 }
 
 
-function Perceptron(input, hidden, output)
-{
+function Perceptron(input, hidden, output) {
     //Need this constants for work
-    const { Layer } = window.synaptic;
-	// create the layers
-	var inputLayer = new Layer(input);
-	var hiddenLayer = new Layer(hidden);
-	var outputLayer = new Layer(output);
+    const {
+        Layer
+    } = window.synaptic;
+    // create the layers
+    var inputLayer = new Layer(input);
+    var hiddenLayer = new Layer(hidden);
+    var outputLayer = new Layer(output);
 
-	// connect the layers
-	inputLayer.project(hiddenLayer);
-	hiddenLayer.project(outputLayer);
+    // connect the layers
+    inputLayer.project(hiddenLayer);
+    hiddenLayer.project(outputLayer);
 
-	// set the layers
-	this.set({
-		input: inputLayer,
-		hidden: [hiddenLayer],
-		output: outputLayer
-	});
+    // set the layers
+    this.set({
+        input: inputLayer,
+        hidden: [hiddenLayer],
+        output: outputLayer
+    });
 }
-
--- assets/js/perceptronMethodSimple.js
+++ assets/js/perceptronMethodSimple.js
@@ -1,10 +1,11 @@
 //Global values
-var threshold, downZero, upZero, resultZ = 0, erroResult, count = 1;
+var threshold, downZero, upZero, resultZ = 0,
+    erroResult, count = 1;
 var entriesValues = new Array;
 var exitWish = new Array;
 var height = new Array;
 
-function addValues(){
+function addValues() {
     threshold = 0.5; //Bias
     downZero = -1;
     upZero = 1;
@@ -13,50 +14,49 @@ function addValues(){
     height = [0, -8, 2, 4];
 }
 
-function perceptron(){
+function perceptron() {
     addValues();
-   do{
+    do {
         resultZ = calculateZ();
         erroResult = 0;
-        if (resultZ >= 0){
+        if (resultZ >= 0) {
             errorResult = error(upZero);
-        }
-        else if (resultZ < 0 ){
+        } else if (resultZ < 0) {
             errorResult = error(downZero);
         }
         console.log("Error: " + errorResult);
         newHeight(errorResult);
         console.log("Interacción: " + count);
-        count ++;
+        count++;
         console.log(resultZ <= exitWish)
-    }while (resultZ <= exitWish);
+    } while (resultZ <= exitWish);
 }
 
 //yd = exit wish, yob = downZero or upZero
-function error(yob){
+function error(yob) {
     var e = exitWish - yob;
     return e;
     console.log(e);
 }
 
-function calculateZ(){
+function calculateZ() {
     var result = 0;
-    for (var i = 0; i <= entriesValues.length - 1; i++){
+    for (var i = 0; i <= entriesValues.length - 1; i++) {
         result = result + (entriesValues[i] * height[i]);
     }
     console.log("Result: " + result);
     return result;
 }
 
-function newHeight(errorValue){
+function newHeight(errorValue) {
     var wk = new Array;
-    for (var o = 0; o <= height.length - 1; o++){
+    for (var o = 0; o <= height.length - 1; o++) {
         wk.push(height[o] + ((threshold * errorValue) * entriesValues[o]));
     }
     console.log("New heights");
-    for (var r = 0; r <= height.length - 1; r++){
-        if (wk[r] == height[r]){
-            wk[r] ++;
+    for (var r = 0; r <= height.length - 1; r++) {
+        if (wk[r] == height[r]) {
+            wk[r]++;
         }
     }
     height = [];
